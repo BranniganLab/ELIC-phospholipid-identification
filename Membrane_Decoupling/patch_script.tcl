2b9d9f397b7556101e66f17f316f257a4a6b281d
EzryStIago <ezrysm@gmail.com>
2022-03-21 17:50:12 -0400

    Working patch for arbitrary lipid mutation

--- Membrane_Decoupling/patch_script.tcl
+++ Membrane_Decoupling/patch_script.tcl
@@ -2,56 +2,50 @@ package require topotools
 package require psfgen
 package require alchemify 
 
+#Clean slate
 mol delete all
 resetpsf
+psfcontext reset
 
-topology test.rtf
 topology ../toppar/top_all36_lipid.rtf
+topology test.rtf
 
-mol load psf POPC120.psf pdb POPC120.pdb
-set sel [atomselect top "lipid and resid 5"]
-$sel set resname "POCE"
 
-animate write psf new.psf
-animate write pdb new.pdb
+proc mutateResidue {the_resid patchName prefixin prefixout} {
+	#Rename the target lipid
+	mol load psf $prefixin.psf pdb $prefixin.pdb
+	set sel [atomselect top "lipid and resid $the_resid"]
+	$sel set resname $patchName
+	mol delrep 0 top
 
-readpsf new.psf
-coordpdb new.pdb
+	animate write psf $prefixout.psf
+	animate write pdb $prefixout.pdb
 
-patch POCE MEMB:5
-#guesscoord
+	#Read in the renamed structure
+	readpsf $prefixout.psf
+	coordpdb $prefixout.pdb
 
-writepsf new.psf
-writepdb new.pdb
+	#do patch
+	patch $patchName MEMB:$the_resid
+	guesscoord
+	writepsf $prefixout.psf
+	writepdb $prefixout.pdb
 
-mol delete all
-mol new new.psf
-mol addfile new.pdb
-
+	mol new $prefixout.psf
+	mol addfile $prefixout.pdb
+	mol delrep 0 top 
+	mol selection "resname $patchName"
+	mol addrep top
+}
 
 #Alchemify
-set reset [atomselect top all]
-$reset set beta 0
-set alchin [atomselect top "resname POCE and name HN1M HN2M HN3M NM H12V C12M H12W H11W C11M H11V O12M PM O14M O13M O11M"]
-$alchin set beta 1
-set alchout [atomselect top "resname POCE and name C15 H15A H15B H15C C14 H14A H14B H14C C13 H13B H13A H13C N H12B C12 H12A H11B C11 H11A O12 P O14 O13 O11"]
-$alchout set beta -1
-animate write pdb new.fep.pdb
+proc doAlch {patchName prefixout} {
+	set alchin [atomselect top "occupancy 0"] ;#Take advantage of the fact that guessed coordinates have occ=0
+	$alchin set beta 1
+	set alchout [atomselect top "resname $patchName and name C15 H15A H15B H15C C14 H14A H14B H14C C13 H13B H13A H13C N H12B C12 H12A H11B C11 H11A O12 P O14 O13 O11"]
+	$alchout set beta -1
 
-alchemify new.psf new.fep.psf new.fep.pdb
+	animate write pdb $prefixout.fep.pdb
+	alchemify $prefixout.psf $prefixout.fep.psf $prefixout.fep.pdb
+}
 
-mol delete all
-resetpsf
-
-readpsf new.fep.psf
-coordpdb new.fep.pdb
-mol new new.fep.psf
-mol addfile new.fep.pdb
-guesscoord
-
-return
-
-writepsf new.guessed.psf
-writepdb new.guessed.pdb
-mol new new.guessed.psf
-mol addfile new.guessed.pdb
