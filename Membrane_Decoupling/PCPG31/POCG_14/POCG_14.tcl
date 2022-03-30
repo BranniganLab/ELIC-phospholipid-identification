fd38255f689af41942f4710966ef9173d1e981e7
EzryStIago <ezrysm@gmail.com>
2022-03-30 15:44:04 -0400

    making replicas

---
+++ Membrane_Decoupling/PCPG31/POCG_14/POCG_14.tcl
@@ -0,0 +1,10 @@
+
+source patch_script.tcl
+set res 14
+set patch POCG
+set prefin PCPG31
+set prefout POCG_14
+set namesOut {C15 H15A H15B H15C C14 H14A H14B H14C C13 H13B H13A H13C N H12B C12 H12A H11B C11 H11A O12 P O14 O13 O11}
+main $res $patch $prefin $prefout $namesOut
+package require cv_dashboard
+cv load ./POCG_14/zrestraint.POCG_14.colvars
