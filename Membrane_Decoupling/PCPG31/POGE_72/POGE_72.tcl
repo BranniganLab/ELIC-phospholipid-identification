6ca38693ae103f9c1d3e6661ea591c0bfa0f2492
EzryStIago <ezrysm@gmail.com>
2022-05-04 16:38:17 -0400

    cleanup and new replicas. WARNING: not all zrestraints are correct

---
+++ Membrane_Decoupling/PCPG31/POGE_72/POGE_72.tcl
@@ -0,0 +1,9 @@
+
+source ../../common/patch_script.tcl
+set res 72
+set patch POGE
+set prefin PCPG31
+set prefout POGE_72
+set namesOut {O11 P O13 O14 O12 C11 H11B H11A H12A C12 OC2 HO2 C13 H13A H13B OC3 HO3}
+set commonPath ../../common
+main $res $patch $prefin $prefout $namesOut $commonPath
